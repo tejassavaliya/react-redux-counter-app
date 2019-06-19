9f41011179d9e627c45b105892ca8a3069acd10c
tejas.s <tejas.s@ezdi.us>
2019-06-19 13:03:31 +0530

    added functionality for age counter up and down

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        age:21
+    };
+    onAgeUp = () => {
+      this.setState({
+          ...this.state.age,
+          age: ++this.state.age
+      })
+    };
+    onAgeDown = () => {
+      this.setState({
+          ...this.state,
+          age: --this.state.age
+      })
+    };
+    render () {
+        return (
+            <div className="App">
+                <div>Age: <span>{this.state.age}</span></div>
+                <button onClick={this.onAgeUp}>Age Up</button>
+                <button onClick={this.onAgeDown}>Age Down</button>
+            </div>
+        );
+    }
 }
 
 export default App;
